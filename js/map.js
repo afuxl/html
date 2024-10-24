2486ec50e51f4c64d167db0ccb536a30dc97d2fd
afuxl <146636868+afuxl@users.noreply.github.com>
2024-10-24 19:44:11 +0800

    Update map.js

--- js/map.js
+++ js/map.js
@@ -177,7 +177,7 @@ function countVisibleShips() {
         }
     });
     // Update teks jumlah kapal yang terlihat
-    document.getElementById('ship-count').innerText = `TOTAL VISIBLE SHIPS: ${visibleMarkers} From ${totalShips}`;
+    document.getElementById('ship-count').innerText = `Total visible ship: ${visibleMarkers} From ${totalShips}`;
 }
 
 // Event listener untuk memperbarui jumlah kapal terlihat saat peta digerakkan atau di-zoom
